e6b72bee0eb5741209f9a6f7f60389300f890d4b
ShakedBar66t <alive2154@gmail.com>
2024-01-19 17:13:33 +0200

    small changes

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 
 export default function Footer() {
     return <footer className="w-full py-4 bg-primary-500">
-        <small className="w-full block text-center text-gray-20">Built and designed by Elon Hahomo & Shaked Barsheshet @2023</small>
+        <small className="w-full block text-center text-gray-20">Built and designed by Elon B & Shaked Barsheshet @2023</small>
     </footer>
 }
